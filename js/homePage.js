4aa0ef9999a36a4cbe48ee75a3dd6adf743f6ad1
nam <29ngtrung@gmail.com>
2025-03-04 01:51:53 +0700

    Trang chu v2 - the thao section 30%

--- js/homePage.js
+++ js/homePage.js
@@ -12,7 +12,7 @@ class Article {
 
 let danhSachArticle = [
     new Article("Nvidia reports earnings today. Here's what to expect after the DeepSeek shock", "Nvidia (NVDA) stock advanced by more than 3% ahead of the AI chip giant's scheduled release of its fiscal fourth-quarter earnings after the close today. The shares of chip designers Broadcom (AVGO) and Micron (MU) also gained."),
-    new Article("Ông Zelensky sẵn sàng gặp lại ông Trump để 'nói chuyện nghiêm túc", "Tổng thống Ukraine Zelensky nói sẵn sàng gặp lại người đồng cấp Mỹ nếu được mời, nhưng cho hay đó phải là cuộc thảo luận nghiêm túc."),
+    new Article("Ông Zelensky sẵn sàng gặp lại ông Trump để 'nói chuyện nghiêm túc", "Tổng thống Ukraine Zelensky nói sẵn sàng gặp lại người đồng cấp Mỹ nếu được mời, nhưng cho hay đó phải là cuộc thảo luận nghiêm túc. "),
     new Article("Elon Musk nói về khả năng AI tiêu diệt nhân loại", "Tỷ phú Elon Musk cho rằng AI thông minh hơn con người là mối đe dọa hiện hữu dù chỉ có 20% khả năng tiêu diệt nhân loại.")
 ];
 
@@ -21,5 +21,4 @@ myCarousel.addEventListener('slide.bs.carousel', event => {
     let activeIndex = event.to;
     bigNewsHeading.textContent = danhSachArticle[activeIndex].articleHeading;
     bigNewsArticle.textContent = danhSachArticle[activeIndex].articleParagraph;
-    // document.getElementById('carouselText').innerText = textList[activeIndex];
 })
