d022e52aa56f793c6ab3044faaf6d13204e5b01d
nam <29ngtrung@gmail.com>
2025-03-08 23:20:54 +0700

    Done chuyen trang lvl1 cho burgerIcon

--- js/pageWorkFlow.js
+++ js/pageWorkFlow.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const subMenuParent = document.querySelectorAll(".sub-menu-parent a")
+    const burgerIconSubMenuParent = document.querySelectorAll(".cat-set-big-title")
+
+    function chuyenTrang(name) {
+        const categoryName = name.textContent;
+
+        window.localStorage.setItem("category-name", categoryName);
+        window.location.href = "../html/trangDanhMuc.html";
+    }
 
     for ( let i = 0; i < subMenuParent.length; i++ ) {
         subMenuParent[i].addEventListener("click", function(e) {
             e.preventDefault();
-            const categoryName = subMenuParent[i].textContent;
-            console.log(categoryName);
+            chuyenTrang(subMenuParent[i])
+        })
+    }
 
-            window.localStorage.setItem("category-name", categoryName);
-            window.location.href = "../html/trangDanhMuc.html";
+    for ( let i = 0; i < burgerIconSubMenuParent.length; i++ ) {
+        burgerIconSubMenuParent[i].addEventListener("click", function(e) {
+            e.preventDefault();
+            chuyenTrang(burgerIconSubMenuParent[i])
         })
     }
 
