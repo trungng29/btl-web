42ce70755d6673d6dce1fbf3a6fd124379314bf2
nam <29ngtrung@gmail.com>
2025-04-16 23:18:01 +0700

    API article latest

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,56 +1,56 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import { executeQuery } from "../config/db.js";
-import bodyParser from 'body-parser';
-import dayjs from 'dayjs';
-import 'dayjs/locale/vi.js'; // Import tiếng Việt
+import bodyParser from "body-parser";
+import dayjs from "dayjs";
+import "dayjs/locale/vi.js"; 
 
-dayjs.locale('vi'); // Đặt ngôn ngữ tiếng Việt
+dayjs.locale("vi"); 
 
-// Configuration
-cloudinary.config({ 
-    cloud_name: 'drh4upxz5', 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+  cloud_name: "drh4upxz5",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
-    
-// (async function() {
-//     const result = await cloudinary.uploader.upload("../public/assets/test2.jpeg")
-//     console.log(result);
-// })();
-
-
-
 
 export const articleController = {
-    getArticles: async (req, res) => {
-        const query = `SELECT * FROM [dbo].[Article]`;
-        const values = [];
-        const paramNames = [];
-        const isStoredProcedure = false;
-        try {
-            const result = await executeQuery( query, values, paramNames, isStoredProcedure );
-            return result.recordset;
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
-        }
-    },
+  getArticles: async (req, res) => {
+    const query = `SELECT * FROM [dbo].[Article]`;
+    const values = [];
+    const paramNames = [];
+    const isStoredProcedure = false;
+    try {
+      const result = await executeQuery(
+        query,
+        values,
+        paramNames,
+        isStoredProcedure
+      );
+      return result.recordset;
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
+  },
 
-    transportArticle: async (req, res) => {
-        const articleId = req.params.id;
-        const query = `SELECT * FROM [dbo].[Article] WHERE name_alias = @id`;
-        const values = [articleId];
-        const paramNames = ["id"];
-        const isStoredProcedure = false;
-        let result;
-        try {
-            result = await executeQuery( query, values, paramNames, isStoredProcedure );
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
-        }
+  transportArticle: async (req, res) => {
+    const articleId = req.params.id;
+    const query = `SELECT * FROM [dbo].[Article] WHERE name_alias = @id`;
+    const values = [articleId];
+    const paramNames = ["id"];
+    const isStoredProcedure = false;
+    let result;
+    try {
+      result = await executeQuery(query, values, paramNames, isStoredProcedure);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
 
-        const query2 = `SELECT 
+    const query2 = `SELECT 
                             pr.category_name AS parent_category, 
                             ch.category_name AS child_category
                         FROM 
@@ -58,64 +58,116 @@ export const articleController = {
                         LEFT JOIN 
                             [dbo].[Category] pr ON ch.id_parent = pr.id_category
                         WHERE 
-                            ch.id_category = @id_category;`
-        const values2 = [result.recordset[0].id_category];
-        const paramNames2 = ["id_category"];
-        const isStoredProcedure2 = false;
-        let result2;
-        try {
-            result2 = await executeQuery( query2, values2, paramNames2, isStoredProcedure2 );
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
-        }
+                            ch.id_category = @id_category;`;
+    const values2 = [result.recordset[0].id_category];
+    const paramNames2 = ["id_category"];
+    const isStoredProcedure2 = false;
+    let result2;
+    try {
+      result2 = await executeQuery(
+        query2,
+        values2,
+        paramNames2,
+        isStoredProcedure2
+      );
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
 
-        const query1 = `SELECT username FROM [dbo].[User] WHERE id_user = @id_user`;
-        const values1 = [result.recordset[0].id_user];
-        const paramNames1 = ["id_user"];
-        const isStoredProcedure1 = false;
-        try {
-            const result1 = await executeQuery( query1, values1, paramNames1, isStoredProcedure1 );
-            const rawDate = result.recordset[0].day_created;
-            const formatted = dayjs(rawDate).format('dddd, D/M/YYYY, HH:mm');
-            res.render("chiTietBaiViet.ejs", { 
-                articleDetals: result.recordset[0],
-                userDetals: result1.recordset[0],
-                categoryDetals: result2.recordset[0],
-                formattedDate: formatted})
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
-        }
-    },
+    const query1 = `SELECT username FROM [dbo].[User] WHERE id_user = @id_user`;
+    const values1 = [result.recordset[0].id_user];
+    const paramNames1 = ["id_user"];
+    const isStoredProcedure1 = false;
+    try {
+      const result1 = await executeQuery(
+        query1,
+        values1,
+        paramNames1,
+        isStoredProcedure1
+      );
+      const rawDate = result.recordset[0].day_created;
+      const formatted = dayjs(rawDate).format("dddd, D/M/YYYY, HH:mm");
+      res.render("chiTietBaiViet.ejs", {
+        articleDetals: result.recordset[0],
+        userDetals: result1.recordset[0],
+        categoryDetals: result2.recordset[0],
+        formattedDate: formatted,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
+  },
 
-    searchArticles: async (req, res) => {
-        const query = `SELECT * FROM [dbo].[Article]
+  searchArticles: async (req, res) => {
+    const query = `SELECT * FROM [dbo].[Article]
                     WHERE heading COLLATE Latin1_General_CI_AI LIKE '%' + @id + '%';`;
-        const values = [`%${req.body.navbarTrenSb}%`]; // Đưa dấu % vào giá trị
-        const paramNames = ["id"];
-    
-        try {
-            const result = await executeQuery(query, values, paramNames, false);
-            res.json({ success: true, data: result.recordset }); // Gửi lại kết quả nếu đúng
-        } catch (error) {
-            console.error(error);
-            res.render('notFound404.ejs');
-        }
-    },
+    const values = [`%${req.body.navbarTrenSb}%`]; // Đưa dấu % vào giá trị
+    const paramNames = ["id"];
 
-    getArticles1: async (req, res) => {
-            const query = `SELECT * FROM [dbo].[Article]`;
-            const values = [];
-            const paramNames = [];
-            const isStoredProcedure = false;
-            try {
-                const result = await executeQuery( query, values, paramNames, isStoredProcedure );
-                // return result.recordset;
-                res.json( { success: true, data: result.recordset });
-            } catch(error) {
-                console.error(error);
-                res.status(500).json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
-            }
-        },
+    try {
+      const result = await executeQuery(query, values, paramNames, false);
+      res.json({ success: true, data: result.recordset }); // Gửi lại kết quả nếu đúng
+    } catch (error) {
+      console.error(error);
+      res.render("notFound404.ejs");
+    }
+  },
+
+  getArticles1: async (req, res) => {
+    const query = `SELECT * FROM [dbo].[Article]`;
+    const values = [];
+    const paramNames = [];
+    const isStoredProcedure = false;
+    try {
+      const result = await executeQuery(
+        query,
+        values,
+        paramNames,
+        isStoredProcedure
+      );
+      // return result.recordset;
+      res.json({ success: true, data: result.recordset });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
+  },
+
+  getArticlesOldest: async (req, res) => {
+
+    const subQuery = `SELECT Category.id_category FROM Category WHERE Category.alias_name = @id`
+    const subValues = [req.params.id];
+    const subParamName = ['id'];
+
+    try {
+        const subResult = await executeQuery(subQuery, subValues, subParamName, false);
+        console.log(subResult.recordset)
+        const query = `SELECT *
+                FROM Article WHERE id_category = @id
+                ORDER BY day_created ASC;`
+        const values = [subResult.recordset[0].id_category];
+        const paramNames = ['id'];
+        const result = await executeQuery(query, values, paramNames, false);
+        res.json({ success: 'Thanh cong !', name: subResult.recordset, data: result.recordset})
+    } catch (error) {
+        console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Có lỗi xảy ra, vui lòng thử lại!" });
+    }
+
+    
+
+   
+  },
+
+
 };
--- routes/getArticle.js
+++ routes/getArticle.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/getarticle", articleController.getArticles);
 router.get("/transportArticle/:id", articleController.transportArticle);
-router.get("/searchArticle", articleController.searchArticles)
+router.get("/searchArticle", articleController.searchArticles);
+router.get("/getArticlesOldest/:id", articleController.getArticlesOldest);
 
 export { router }
