345fcbbc4c585de4a8f2bd81792183304a5fd122
nam <29ngtrung@gmail.com>
2025-04-16 22:51:37 +0700

    Fix api upload image - done

--- controllers/uploadPicturesController.js
+++ controllers/uploadPicturesController.js
@@ -1,13 +1,13 @@
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
-import fs from 'fs';
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-cloudinary.config({ 
-    cloud_name: 'drh4upxz5', 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+  cloud_name: "drh4upxz5",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 /**
@@ -17,21 +17,21 @@ cloudinary.config({
  * @returns {Promise<object>} Upload result.
  */
 
-export async function uploadImageToCloudinary(filePath, folder = 'heroImages') {
-    try {
-      const result = await cloudinary.uploader.upload(filePath, { folder });
-      fs.unlinkSync(filePath); // Xoá file sau khi upload
-  
-      return {
-        success: true,
-        url: result.secure_url,
-        public_id: result.public_id,
-      };
-    } catch (error) {
-      console.error('Upload error:', error);
-      return {
-        success: false,
-        message: 'Failed to upload image',
-      };
-    }
+export async function uploadImageToCloudinary(filePath, folder = "heroImages") {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, { folder });
+    fs.unlinkSync(filePath); // Xoá file sau khi upload
+
+    return {
+      success: true,
+      url: result.secure_url,
+      public_id: result.public_id,
+    };
+  } catch (error) {
+    console.error("Upload error:", error);
+    return {
+      success: false,
+      message: "Failed to upload image",
+    };
   }
+}
